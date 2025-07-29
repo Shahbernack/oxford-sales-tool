d6197a9bf7d3e7ab8c295a1354fef2ac63237e6c
Shahbernack <s@abshenasan.de>
2025-07-29 15:16:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ authenticator = stauth.Authenticate(
     key="oxford_signature",
     cookie_expiry_days=1
 )
-name, auth_status, username = authenticator.login("Login", "sidebar")
+name, auth_status, username = authenticator.login("Login", "main")
 
 if auth_status is False:
     st.error("Username/password is incorrect")
